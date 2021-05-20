374da8c3df0edf757d81997faa121c52381a4303
Kelly Netterville <bitgrease@gmail.com>
2021-05-20 17:24:43 -0400

    Added eventlistener for click and display number chosen. Also, check to make sure a valide number was provided.

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
+// document.querySelector('.score').textContent = 21;
+// document.querySelector('.number').textContent = 13;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value)
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
+
+    if(!guess) {
+        document.querySelector('.message').textContent = '👎 No number provided!';
+    } else {
+        document.querySelector('.number').textContent = guess;
+    }
+})
