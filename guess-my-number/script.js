b6a47c8d7b43340388485ba9707838172c6c80ee
Kelly Netterville <bitgrease@gmail.com>
2021-05-24 13:41:26 -0400

    Added logic to create random number and then compare against user guess and update message if guess is correct.

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -7,14 +7,15 @@
 
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value)
-
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess);
 
     if(!guess) {
         document.querySelector('.message').textContent = '👎 No number provided!';
-    } else {
-        document.querySelector('.number').textContent = guess;
+    } else if(guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct Number!'   
     }
 })
