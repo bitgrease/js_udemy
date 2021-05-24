96bf3377fda969d929241c5d36b77be3e0253f5a
Kelly Netterville <bitgrease@gmail.com>
2021-05-24 15:24:25 -0400

    Fixed logic error where score var was declared inside eventListner for "click" causing it to be reset on every click.

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -8,18 +8,23 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value)
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
-    console.log(guess);
-
     if(!guess) {
         document.querySelector('.message').textContent = '👎 No number provided!';
     } else if(guess === secretNumber) {
-        document.querySelector('.message').textContent = '🎉 Correct Number!'   
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
     } else if(guess > secretNumber) {
-        document.querySelector('.message').textContent = '🔺 Number is TOO HIGH!'
+        score -= 1;
+        console.log(score, guess);
+        document.querySelector('.message').textContent = '🔺 Number is TOO HIGH!';
+        document.querySelector('.score').textContent = score;
     } else if(guess < secretNumber) {
-        document.querySelector('.message').textContent = '🔻 Number is TOO LOW!'
+        score -= 1;
+        console.log(score, guess);
+        document.querySelector('.message').textContent = '🔻 Number is TOO LOW!';
+        document.querySelector('.score').textContent = score;
     }
-})
+});
