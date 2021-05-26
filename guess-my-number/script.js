563573bed5147e8d2a087533de96609eed2970d5
Kelly Netterville <bitgrease@gmail.com>
2021-05-26 14:05:06 -0400

    Moved line to reveal number to success condition branch.

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -11,7 +11,6 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
-document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     if(score > 1) { 
@@ -21,6 +20,7 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('body').style.backgroundColor = '#60b347';
             document.querySelector('.number').style.width = '30rem';
             document.querySelector('.message').textContent = '🎉 Correct Number!';
+            document.querySelector('.number').textContent = secretNumber;
         } else if(guess > secretNumber) {
             score -= 1;
             document.querySelector('.message').textContent = '🔺 Number is TOO HIGH!';
