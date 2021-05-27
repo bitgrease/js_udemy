0e3cf4e7522dc346ac9f15fb2b2143b7f65ba5a7
Kelly Netterville <bitgrease@gmail.com>
2021-05-26 20:08:54 -0400

    Added logic to store high score, reset game and hide random number during play.

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -7,9 +7,9 @@
 
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value)
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-let highScore = 0;
+const initialMessage = 'Start guessing...';
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -21,6 +21,10 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.number').style.width = '30rem';
             document.querySelector('.message').textContent = '🎉 Correct Number!';
             document.querySelector('.number').textContent = secretNumber;
+            if (Number(document.querySelector('.highscore').textContent) < Number(score)) {
+                console.log(`Setting Highscore: ${document.querySelector('.highscore').textContent} to ${score}`)
+                document.querySelector('.highscore').textContent = score;
+            }
         } else if(guess > secretNumber) {
             score -= 1;
             document.querySelector('.message').textContent = '🔺 Number is TOO HIGH!';
@@ -36,4 +40,13 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('.message').textContent = '💥 You lost the game.';
         document.querySelector('.score').textContent = score;
     }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    document.querySelector('.message').textContent = initialMessage;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15';
+    document.querySelector('body').style.background = "#222"
 });
