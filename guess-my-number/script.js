2ebc63fbe7033388f8386cad1cbbde77da3e1ced
Kelly Netterville <bitgrease@gmail.com>
2021-05-28 18:39:36 -0400

    Fixed issue with score not resetting when Again is clicked.

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -49,4 +49,5 @@ document.querySelector('.again').addEventListener('click', function() {
     document.querySelector('.number').textContent = '?';
     document.querySelector('.number').style.width = '15';
     document.querySelector('body').style.background = "#222"
+    document.querySelector('.score').textContent = score;
 });
