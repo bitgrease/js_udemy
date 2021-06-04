22c902d9316ddafb751fe5123c11be7b4efe24eb
Kelly Netterville <bitgrease@gmail.com>
2021-06-03 20:47:48 -0400

    Extract code to hide modal window and overlay to a function.

--- modal_window/script.js
+++ modal_window/script.js
@@ -6,6 +6,11 @@ const overlay = document.querySelector('.overlay');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 console.log(btnsShowModal);
 
+const hideModalandOverlay = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
 btnsShowModal.forEach(btn => {
     btn.addEventListener('click', function() {
         // Can remove multiple classes as comma separated arguments
@@ -14,7 +19,6 @@ btnsShowModal.forEach(btn => {
     })
 });
 
-btnCloseModal.addEventListener('click', function() {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden')
-});
+btnCloseModal.addEventListener('click', hideModalandOverlay);
+
+overlay.addEventListener('click', hideModalandOverlay);
