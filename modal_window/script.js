6bc2933dc0b5776075f292d1c13a26a74cb6ab16
Kelly Netterville <bitgrease@gmail.com>
2021-06-03 20:55:04 -0400

    Extract code to display modal window to a function.

--- modal_window/script.js
+++ modal_window/script.js
@@ -9,16 +9,15 @@ console.log(btnsShowModal);
 const hideModalandOverlay = function() {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
-}
+};
 
+const showModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
 btnsShowModal.forEach(btn => {
-    btn.addEventListener('click', function() {
-        // Can remove multiple classes as comma separated arguments
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    })
+    btn.addEventListener('click', showModal);
 });
 
 btnCloseModal.addEventListener('click', hideModalandOverlay);
-
 overlay.addEventListener('click', hideModalandOverlay);
